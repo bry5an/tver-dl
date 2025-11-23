5ae1a672ab92b3a218b58f1ebc7d12ac9b062dff
Bryan Banks <bry5an@gmail.com>
2025-11-22 19:24:30 -0800

    docs: update README with installation, usage, CLI options, and YAML config; chore: bump version to 0.2.0

--- tver_dl/_version.py
+++ tver_dl/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev16+ga0112ae8e.d20251122'
-__version_tuple__ = version_tuple = (0, 1, 'dev16', 'ga0112ae8e.d20251122')
+__version__ = version = '0.1.3'
+__version_tuple__ = version_tuple = (0, 1, 3)
 
 __commit_id__ = commit_id = None
